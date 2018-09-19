8c157b813857926b35cfc3feb768128ebb9dbab9
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-19 10:32:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,6 +12,7 @@ var chocolateBars = ["snickers", "hundredgrand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(arrayName, elementName){
  var arrayName = new Array();
  var arrayName =[...arrayName, elementName]
+ retrun arrayName;
 }
 
 function destructivelyAddElementToBeginningOfArray(arrayName, elementName)

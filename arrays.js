6155704083349d18b7c3cfdff1763185fac55801
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-19 11:24:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -47,8 +47,8 @@ function removeElementFromBeginningOfArray(array)
    
  function destructivelyRemoveElementFromEndOfArray(array)
  {
-   array.pop();
-   return array;
+  return array.pop();
+   
  }
    
   function removeElementFromEndOfArray(array)
